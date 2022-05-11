cc3320199f711bb6edd7a1260e6ccf4cba15ca49
paolaveliez <graficaviamonte@yahoo.com.ar>
2022-05-11 18:35:36 -0300

    Nuevo commit

---
+++ BACKEND/src/test/java/com/portfolio/Paola/PaolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Paola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
