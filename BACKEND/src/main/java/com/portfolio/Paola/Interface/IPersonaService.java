8d001ef06095458cbff13fb0d4b3196e52280322
paolaveliez <graficaviamonte@yahoo.com.ar>
2022-05-16 20:17:42 -0300

    commit

---
+++ BACKEND/src/main/java/com/portfolio/Paola/Interface/IPersonaService.java
@@ -0,0 +1,20 @@
+
+package com.portfolio.Paola.Interface;
+
+import com.portfolio.Paola.Entity.Persona;
+import java.util.List;
+
+
+public interface IPersonaService {
+  //Traer una lista de persona
+    public List<Persona> getPersona();
+    //Guardar  un objeto de tipo persona
+    public void savePersona(Persona persona);
+    //Eliminar un objeto pero lo buscamos por ID
+    public void deletePersona (Long id);
+    //Buscar una persona por ID
+    public Persona findPersona(Long id);
+    
+}
+  
+
---
+++ BACKEND/src/main/java/com/portfolio/Paola/Repository/IPersonaRepository.java
@@ -0,0 +1,12 @@
+
+package com.portfolio.Paola.Repository;
+
+import com.portfolio.Paola.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface IPersonaRepository extends JpaRepository <Persona,Long> {
+    
+}
