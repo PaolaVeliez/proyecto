75f682ee59fa123adc082be0a3269af2ad412b43
paolaveliez <graficaviamonte@yahoo.com.ar>
2022-05-25 19:58:19 -0300

    commit

--- BACKEND/src/main/java/com/portfolio/Paola/Controller/PersonaController.java
+++ BACKEND/src/main/java/com/portfolio/Paola/Controller/PersonaController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin (origins = "htpp//:localhost4200")
+@CrossOrigin (origins ="http://localhost4200")
 
 public class PersonaController {
     
